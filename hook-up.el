82972c682d710fa92d749edf765ecc14bf287844
Keith Wygant <kawygant@ncsu.edu>
2017-07-07 16:44:38 -0400

    updated package requirements.
    I also reformatted things a bit.

--- hook-up.el
+++ hook-up.el
@@ -1,17 +1,14 @@
 ;;; hook-up.el --- Boom Boom Boom Boom -*- lexical-binding: t -*-
-(eval-when-compile
-  (require 'umr))
-(require 'miscellaneous)
-;; (require 'seq) ; required by `umr'
+(mapc #'require
+      [miscellaneous seq])
 
 (defmacro hook-up-make-hook (WHEN PROCEDURE &rest CONTINGENT)
-  "Set up a hook to run WHEN PROCEDURE.
-    Create variable WHEN-PROCEDURE-hook and assign it the value CONTINGENT.
-    Create function WHEN-PROCEDURE-hook to run WHEN PROCEDURE-hook using `run-hooks'.
-    Use `advice-add' to add run-WHEN-PROCEDURE-hook as advice to PROCEDURE."
+  "Set up a hook to run WHEN PROCEDURE:
+Create variable WHEN-PROCEDURE-hook and assign it the value CONTINGENT.
+Create function WHEN-PROCEDURE-hook to run WHEN PROCEDURE-hook using `run-hooks'.
+Use `advice-add' to add run-WHEN-PROCEDURE-hook as advice to PROCEDURE."
   (declare (indent 2))
-  (umr-let
-   hook (misc--symb WHEN "-" PROCEDURE "-hook")
+  (let ((hook (misc--symb WHEN "-" PROCEDURE "-hook")))
    `(progn
       (defvar ,hook ',CONTINGENT
         ,(misc--mkstr "procedures to run " WHEN " `" PROCEDURE "'"))
@@ -24,8 +21,8 @@
 
 (defun hook-up (HOOKS FUNCTIONS &rest FLAGS)
   "Hang all FUNCTIONS, in order, on all HOOKS.
-If FLAGS includes :append, the functions are added to the end of the hook.
-If FLAGS includes :local, the functions are added to the buffer-local hook."
+If FLAGS includes :append, the functions are hanged on the end of the hook.
+If FLAGS includes :local, the functions are hanged on the buffer-local hook."
   (seq-doseq (h HOOKS)
     (seq-doseq (f (seq-reverse FUNCTIONS))
       (add-hook h f (memq :append FLAGS) (memq :local FLAGS)))))
